5f1119942df634931ef81c096f6b8b4e4eb152a1
DEVELOPDAF <108474386+DEVELOPDAF@users.noreply.github.com>
2022-08-04 18:55:08 +0200

    Update ajax-utils.js

--- js/ajax-utils.js
+++ js/ajax-utils.js
@@ -6,12 +6,12 @@ var ajaxUtils = {};
 
 // Returns an HTTP request object
 function getRequestObject() {
-  if (window.XMLHttpsRequest) {
-    return (new XMLHttpsRequest());
+  if (window.XMLHttpRequest) {
+    return (new XMLHttpRequest());
   } 
   else if (window.ActiveXObject) {
     // For very old IE browsers (optional)
-    return (new ActiveXObject("Microsoft.XMLHTTPS"));
+    return (new ActiveXObject("Microsoft.XMLHTTP"));
   } 
   else {
     global.alert("Ajax is not supported!");
